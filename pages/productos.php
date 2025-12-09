b6046ae08f9f9110450096378585b5a2843c7343
Jhoanis_rq <Jhoanisrq@gmail.com>
2025-12-08 21:23:36 -0500

    Add files via upload

---
+++ pages/productos.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include '../include/db_connect.php';
+include '../include/header.php';
+?>
+<head>
+    <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/productos.css">
+    <link rel="stylesheet" href="../assets/css/modal.css">
+</head>
+<main>
+    <div class="productos-grid"></div>
+</main>
+<script src="../assets/js/productos.js"></script>
